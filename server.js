89d5c8ce6b7459b071b80741cc3baf7d6edfb0ac
LeMaliX <galford.slash.samsho4@gmail.com>
2019-12-28 19:59:34 -0600

    DB configuration

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conected to DB'))
+  .catch((error) => (console.error(error)));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
