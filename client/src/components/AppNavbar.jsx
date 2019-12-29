7f53e424bef59d95bd49db21cf327deb8141a994
LeMaliX <galford.slash.samsho4@gmail.com>
2019-12-29 00:43:28 -0600

    Client setup and navbar implementation

---
+++ client/src/components/AppNavbar.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    isOpen: false,
+  };
+
+  toggle = () => {
+    const { isOpen } = this.state;
+    this.setState({
+      isOpen: !isOpen,
+    });
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/LeMalix/ShoppingList" target="_blank">
+                    GitHub
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
